cd74a7297de28e4b761c51072067663e0331f3c6
kerpyyy <81295217+kerpyyy@users.noreply.github.com>
2021-09-17 14:42:15 +0200

    Delete templates directory

--- templates/job2.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Ficher phtml spéficique à la page 
-$template = 'job2';
-
-// Chargement du layout qui va lui-même charger le template au bon endroit
-require '../templates/layouthome.phtml';
